fc0a71efafeee6c4b9193d60d2369e4b378833ea
Karla Goo <karla_goo@yahoo.com>
2021-07-18 21:34:39 -0500

    added temp

--- assets/script.js
+++ assets/script.js
@@ -4,26 +4,20 @@ var apiKey = "91b227cfb38384f6cbe87310fda24986";
 var currentWeather = document.getElementById("current-weather")
 var previousCities = document.getElementById("previous-search")
 var previousSearch = localStorage.getItem('City')
-console.log(previousSearch);
-
-// if (localStorage.getItem('Previous Searches')) {
-//   console.log("local storage already set");
-//   previousSearches = localStorage.getItem("Previous Searches")
-// }
 
 function renderSearchHistory() {
   console.log("check local storage");
-  for (let i = 0; i < previousSearchesArray.length; i ++) {
+  for (let i = 0; i < previousSearchesArray.length; i++) {
     var previousCity = document.createElement("button");
     previousCity.textContent = previousSearchesArray[i];
-    previousCity.addEventListener("click", function() {
+    previousCity.addEventListener("click", function () {
       getCurrentWeather(previousSearchesArray[i])
     });
     previousCities.append(previousCity);
   }
 };
 
-function updateHistory (city) {
+function updateHistory(city) {
   if (previousSearchesArray.indexOf(city) != -1) {
     return
   }
@@ -40,51 +34,58 @@ function getSearchHistory() {
 };
 
 function getCurrentWeather(city) {
-    cityName = $("#searchBar").val() ? $("#searchBar").val() : city;
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + apiKey;
+  cityName = $("#searchBar").val() ? $("#searchBar").val() : city;
+  var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + apiKey;
 
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
     })
-    .then(function(data) {
-      console.log(data);
+    .then(function (data) {
+      // console.log(data)
       updateHistory(cityName);
-        var lat = data.coord.lat;
-        var lon = data.coord.lon;
-        currentWeather.innerHTML = ""
-        forecast(lat, lon);
-        var nameOfCity =data.name;
+      var lat = data.coord.lat;
+      var lon = data.coord.lon;
+      currentWeather.innerHTML = ""
+      forecast(lat, lon);
+      var nameOfCity = data.name;
 
-        var iconURL = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`
-        var icon = document.createElement('img');
-        var currentConditions = document.createElement("p");
-        var city = document.createElement('p');
-        city.textContent = nameOfCity
-        currentWeather.appendChild(city)
-        currentConditions.textContent = data.weather[0].description
-        currentWeather.appendChild(currentConditions)
-        icon.setAttribute('src', iconURL);
-        currentWeather.appendChild(icon);
-    }).catch(function(err) {
+      var iconURL = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`
+      var icon = document.createElement('img');
+      var currentConditions = document.createElement("p");
+      var city = document.createElement('p');
+      city.textContent = nameOfCity
+      currentWeather.appendChild(city)
+      currentConditions.textContent = data.weather[0].description
+      currentWeather.appendChild(currentConditions)
+      icon.setAttribute('src', iconURL);
+      currentWeather.appendChild(icon);
+    }).catch(function (err) {
       console.log("oh no");
       console.log(err);
     });
 };
 
-function forecast(lat, lon){
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey;
+function forecast(lat, lon) {
+  var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey + '&units=imperial';
 
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
     })
-    .then(function(data) {
+    .then(function (data) {
+      console.log(data)
+      var currentTemp = data.current.temp;
+      var temp = document.createElement('p');
+      console.log(currentTemp)
+      temp.textContent = currentTemp
+      currentWeather.appendChild(temp)
+
     })
 };
 
 $("#searchBtn").on("click", function () {
-    getCurrentWeather();
+  getCurrentWeather();
 })
 
 getSearchHistory();
